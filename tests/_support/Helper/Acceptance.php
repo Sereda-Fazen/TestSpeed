f67b96eed2a3d30696b3919c65fa93c159370dc1
Sereda-fazen <fazen7@mail.ru>
2017-05-12 18:17:37 +0300

    added  124  stages pass

--- tests/_support/Helper/Acceptance.php
+++ tests/_support/Helper/Acceptance.php
@@ -116,7 +116,7 @@ class Acceptance extends \Codeception\Module
 */
     public function writtenDate($host,$dashboard,$content,$product_list,$insight_report,$error)
     {
-        $file = __DIR__.'/servers/Stages.csv';
+        $file = '../TestSpeedPages/servers/Stages.csv';
         $total_time = $dashboard + $content + $product_list + $insight_report;
         $current = file_get_contents($file);
         $date = date("Y-m-d H:i:s");
