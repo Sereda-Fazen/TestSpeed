ef31139c4dc4185d68cb08181b85e8e260ea5c82
Sereda-fazen <fazen7@mail.ru>
2017-05-26 15:50:39 +0300

    test12

--- tests/acceptance/00_SpeedAnalyticsincCest.php
+++ tests/acceptance/00_SpeedAnalyticsincCest.php
@@ -12,20 +12,9 @@ class SpeedAlyticsincCest
         $I->html_header();
     }
     
-    
     function walmartFurniture(\Step\Acceptance\AccountSteps $I)
     {
         $I->stepsSpeedTest('http://walmart-furniture.contentanalyticsinc.com','Walmart-furniture');
-    }
-
-    function acosta(\Step\Acceptance\AccountSteps $I)
-    {
-        $I->stepsSpeedTest('http://acosta.contentanalyticsinc.com','Acosta');
-    }
-
-    function americantextile(\Step\Acceptance\AccountSteps $I)
-    {
-        $I->stepsSpeedTest('http://americantextile.contentanalyticsinc.com', 'Americantextile');
     }
 
     function footer (\Helper\Acceptance $I){
