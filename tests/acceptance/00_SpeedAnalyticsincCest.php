4c8cbe5cd236465cc884fca4484d8cf0ee315d45
Sereda-fazen <fazen7@mail.ru>
2017-05-29 12:28:20 +0300

    5

--- tests/acceptance/00_SpeedAnalyticsincCest.php
+++ tests/acceptance/00_SpeedAnalyticsincCest.php
@@ -8,9 +8,17 @@ use \Step\Acceptance;
 class SpeedAlyticsincCest
 {
 
+    function test (\Helper\Acceptance $I){
+        $I->html_header();
+    }
+
     function walmartFurniture(\Step\Acceptance\AccountSteps $I)
     {
         $I->stepsSpeedTest('http://walmart-furniture.contentanalyticsinc.com','Walmart-furniture');
+    }
+
+    function footer (\Helper\Acceptance $I){
+        $I->html_footer();
     }
 
 
