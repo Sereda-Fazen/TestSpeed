eae75d7975b6581ca3435b3e6436f34d23da434f
Sereda-fazen <fazen7@mail.ru>
2017-06-09 17:22:10 +0300

    added functional test 5

--- tests/functional/RestElastickCest.php
+++ tests/functional/RestElastickCest.php
@@ -13,7 +13,7 @@ class ApiCest
         $I->amHttpAuthenticated('elastic', 'changeme');
         $I->sendGET('_cat/health');
         $I->seeResponseCodeIs(200);
-        $I->sendGET('/_cat/master?format=json');
+        $I->sendGET('_cat/master?format=json');
         $I->seeResponseCodeIs(200);
         $I->seeResponseIsJson();
 
