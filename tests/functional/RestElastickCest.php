7098e4770407e8c2d6ace6acfca52eeb44e22768
Sereda-fazen <fazen7@mail.ru>
2017-06-12 14:54:08 +0300

    added functional test 5

--- tests/functional/RestElastickCest.php
+++ tests/functional/RestElastickCest.php
@@ -29,7 +29,7 @@ class ApiCest
             "message" => "{[\"date\" : \"test\", \"host\" : \"test\", \"dashboard\": \"test\", \"product\": \"test\", \"insight_report\": \"test\", \"status\": \" test\"]}"
 
         ]);
-        $I->canSeeResponseCodeIs(201);
+        $I->canSeeResponseCodeIs(200);
         $I->seeResponseIsJson();
 
 
