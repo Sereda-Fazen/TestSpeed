8faf1d287aa0993d86fd2d3c4511aaa8ba6c0494
Sereda-fazen <fazen7@mail.ru>
2017-06-12 12:45:24 +0300

    added functional test 5

--- tests/functional/RestElastickCest.php
+++ tests/functional/RestElastickCest.php
@@ -14,6 +14,24 @@ class ApiCest
         $I->sendGET('_cat/master?format=json');
         $I->seeResponseCodeIs(200);
         $I->seeResponseIsJson();
+        #$I->sendPUT('http://172.17.0.1:9200/_cat',['attachmentFile' => codecept_data_dir('test.json')]);
+        $I->haveHttpHeader('Content-Type', 'application/json');
+        $I->sendPUT('http://172.17.0.1:9200/_cat', [
+            'settings' => [
+                'index' => [
+                    'number_of_shards' => 3,
+                    'number_of_replicas' => 2
+                ]
+            ]
+        ]);
+        $I->canSeeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+
+
+
+
+
+
         $I->sendGET('_cat/_settings');
         $I->seeResponseCodeIs(200);
         $I->seeResponseIsJson();
