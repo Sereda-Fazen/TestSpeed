ccce03394361548fa12b4af8537b9b8e15bacc15
Sereda-fazen <fazen7@mail.ru>
2017-06-12 13:15:53 +0300

    added functional test 5

--- tests/functional/RestElastickCest.php
+++ tests/functional/RestElastickCest.php
@@ -14,12 +14,30 @@ class ApiCest
         #$I->sendPUT('http://172.17.0.1:9200/_cat',['attachmentFile' => codecept_data_dir('test.json')]);
         $I->haveHttpHeader('Content-Type', 'application/json');
         $I->sendPUT('localhost:9200/_cat/health?pretty', [
-            'settings' => [
-                'index' => [
+            'index' => 'test_index',
+            'body' => [
+                'settings' => [
                     'number_of_shards' => 3,
                     'number_of_replicas' => 2
+                ],
+                'mappings' => [
+                    'my_type' => [
+                        '_source' => [
+                            'enabled' => true
+                        ],
+                        'properties' => [
+                            'first_name' => [
+                                'type' => 'string',
+                                'analyzer' => 'standard'
+                            ],
+                            'age' => [
+                                'type' => 'integer'
+                            ]
+                        ]
+                    ]
                 ]
             ]
+
         ]);
         $I->canSeeResponseCodeIs(200);
         $I->seeResponseIsJson();
