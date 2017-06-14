f4966c639545cce6b75c9a1ecc361d71e945f9a8
Sereda-fazen <fazen7@mail.ru>
2017-06-14 13:42:08 +0300

    remove index - test_index

--- tests/functional/ApiTestCest.php
+++ tests/functional/ApiTestCest.php
@@ -12,7 +12,7 @@ class ApiCest
 
         $I->amHttpAuthenticated('elastic', 'changeme');
         $I->haveHttpHeader('Content-Type', 'application/json');
-
+        $I->sendDELETE('alex_index', []);
 //        $I->sendPUT('alex_index?pretty', [
 //
 //
@@ -41,14 +41,14 @@ class ApiCest
 //        $I->seeResponseCodeIs(200);
 //        $I->seeResponseIsJson();
 
-        $json_data = file_get_contents(__DIR__ . '/../../servers/test.json');
-        $I->sendPUT('alex_index/alex_test/1?pretty', [
-            'user' => 'alex',
-            "post_date" => date("Y-m-d H:i:s"),
-            "message" => addslashes($json_data)]);
-        $I->canSeeResponseCodeIs(201);
-        $I->seeResponseIsJson();
-        $I->comment('The PUT request was sent to Elastic Search!');
+//        $json_data = file_get_contents(__DIR__ . '/../../servers/test.json');
+//        $I->sendPUT('alex_index/alex_test/1?pretty', [
+//            'user' => 'alex',
+//            "post_date" => date("Y-m-d H:i:s"),
+//            "message" => addslashes($json_data)]);
+//        $I->canSeeResponseCodeIs(201);
+//        $I->seeResponseIsJson();
+//        $I->comment('The PUT request was sent to Elastic Search!');
 
 
 
