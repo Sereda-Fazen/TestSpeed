911ae25fade6dfce3554a764271de41ce5d25683
Sereda-fazen <fazen7@mail.ru>
2017-06-14 10:26:53 +0300

    remove index - test_index

--- tests/functional/ApiTestCest.php
+++ tests/functional/ApiTestCest.php
@@ -13,7 +13,7 @@ class ApiCest
         $I->amHttpAuthenticated('elastic', 'changeme');
         $I->haveHttpHeader('Content-Type', 'application/json');
 
-        $I->sendDELETE('/test_index/test/1', [
+        $I->sendDELETE('alex_test_index/alex_type/1', [
 
 //            'index' => 'my_index',
 //            'body' => [
