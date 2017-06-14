b79efd440e49d501eb3bdf3f2fafd8d28b86bea6
Sereda-fazen <fazen7@mail.ru>
2017-06-14 14:17:32 +0300

    remove index - test_index

--- tests/functional/ApiTestCest.php
+++ tests/functional/ApiTestCest.php
@@ -12,63 +12,39 @@ class ApiCest
 
         $I->amHttpAuthenticated('elastic', 'changeme');
         $I->haveHttpHeader('Content-Type', 'application/json');
-//        $I->sendDELETE('alex_index', []);
-        $I->sendPUT('alex_alex_index?pretty', [
-
-                    "settings" => [
-                "number_of_shards" => 1
-        ],
-        "mappings" => [
-                "_default_"=>[
-                    "_timestamp" => [
-                        "enabled" => true,
-                    "store" => true
-                ]
-            ]
-          ]
-        ]);
-
-
-        $I->seeResponseCodeIs(200);
-        $I->seeResponseIsJson();
+       $I->sendDELETE('alex_index', []);
+        $I->sendDELETE('alex_alex_index', []);
+//        $I->sendPUT('alex_alex_index?pretty', [
 //
-        $json_data = file_get_contents(__DIR__ . '/../../servers/test.json');
-        $I->sendPUT('alex_alex_index/alex_test/1?pretty', [
-            'user' => 'alex',
-            "post_date" => date("Y-m-d H:i:s"),
-            "message" => addslashes($json_data),
-            ]);
-        $I->canSeeResponseCodeIs(201);
-        $I->seeResponseIsJson();
-        $I->comment('The PUT request was sent to Elastic Search!');
-
-
-
-//            'index' => 'my_index',
-//            'body' => [
-//                'settings' => [
-//                    'number_of_shards' => 3,
-//                    'number_of_replicas' => 2
-//                ],
-//                'mappings' => [
-//                    'my_type' => [
-//                        '_source' => [
-//                            'enabled' => true
-//                        ],
-//                        'properties' => [
-//                            'first_name' => [
-//                                'type' => 'string',
-//                                'analyzer' => 'standard'
-//                            ],
-//                            'age' => [
-//                                'type' => 'integer'
-//                            ]
-//                        ]
-//                    ]
+//                    "settings" => [
+//                "number_of_shards" => 1
+//        ],
+//        "mappings" => [
+//                "_default_"=>[
+//                    "_timestamp" => [
+//                        "enabled" => true,
+//                    "store" => true
 //                ]
 //            ]
+//          ]
+//        ]);
 //
 //
+//        $I->seeResponseCodeIs(200);
+//        $I->seeResponseIsJson();
+////
+//        $json_data = file_get_contents(__DIR__ . '/../../servers/test.json');
+//        $I->sendPUT('alex_alex_index/alex_test/1?pretty', [
+//            'user' => 'alex',
+//            "post_date" => date("Y-m-d H:i:s"),
+//            "message" => addslashes($json_data),
+//            ]);
+//        $I->canSeeResponseCodeIs(201);
+//        $I->seeResponseIsJson();
+//        $I->comment('The PUT request was sent to Elastic Search!');
+
+
+
 
 
 
