ed8cda697d685015c3d3e91d32cd210639944daf
Sereda-fazen <fazen7@mail.ru>
2017-06-14 12:05:07 +0300

    remove index - test_index

--- tests/functional/ApiTestCest.php
+++ tests/functional/ApiTestCest.php
@@ -13,30 +13,30 @@ class ApiCest
         $I->amHttpAuthenticated('elastic', 'changeme');
         $I->haveHttpHeader('Content-Type', 'application/json');
 
-        $I->sendPUT('alex_index?pretty=true', [
-
-            'settings' => [
-                'index' => [
-                    'number_of_shards' => 1,
-                    'number_of_replicas' => 0
-                ]
-            ]
-
-        ]);
-
-
-        $I->seeResponseCodeIs(200);
-        $I->seeResponseIsJson();
+//        $I->sendPUT('alex_index?pretty=true', [
+//
+//            'settings' => [
+//                'index' => [
+//                    'number_of_shards' => 1,
+//                    'number_of_replicas' => 0
+//                ]
+//            ]
+//
+//        ]);
+//
+//
+//        $I->seeResponseCodeIs(200);
+//        $I->seeResponseIsJson();
 
         $json_data = file_get_contents(__DIR__ . '/../../servers/test.json');
-        $I->sendPUT('alex_index?pretty', [
+        $I->sendPUT('alex_index/alex_test?pretty', [
             'user' => 'alex',
             "post_date" => date("Y-m-d H:i:s"),
             "message" => addslashes($json_data)]);
         $I->canSeeResponseCodeIs(200);
         $I->seeResponseIsJson();
         $I->comment('The PUT request was sent to Elastic Search!');
-        
+
 
 
 //            'index' => 'my_index',
