e6f1e3a68739c426a99bf91501f02f9bfda5d852
Sereda-fazen <fazen7@mail.ru>
2017-06-14 14:18:54 +0300

    remove index - test_index

--- tests/functional/ApiTestCest.php
+++ tests/functional/ApiTestCest.php
@@ -12,36 +12,27 @@ class ApiCest
 
         $I->amHttpAuthenticated('elastic', 'changeme');
         $I->haveHttpHeader('Content-Type', 'application/json');
-       $I->sendDELETE('alex_index', []);
-        $I->sendDELETE('alex_alex_index', []);
-//        $I->sendPUT('alex_alex_index?pretty', [
+ //       $I->sendDELETE('alex_alex_index', []);
+        $I->sendPUT('alex_alex_index?pretty', [
+
+            "settings" => [
+            "number_of_shards" => 1
+        ],
+        ]);
+
+
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
 //
-//                    "settings" => [
-//                "number_of_shards" => 1
-//        ],
-//        "mappings" => [
-//                "_default_"=>[
-//                    "_timestamp" => [
-//                        "enabled" => true,
-//                    "store" => true
-//                ]
-//            ]
-//          ]
-//        ]);
-//
-//
-//        $I->seeResponseCodeIs(200);
-//        $I->seeResponseIsJson();
-////
-//        $json_data = file_get_contents(__DIR__ . '/../../servers/test.json');
-//        $I->sendPUT('alex_alex_index/alex_test/1?pretty', [
-//            'user' => 'alex',
-//            "post_date" => date("Y-m-d H:i:s"),
-//            "message" => addslashes($json_data),
-//            ]);
-//        $I->canSeeResponseCodeIs(201);
-//        $I->seeResponseIsJson();
-//        $I->comment('The PUT request was sent to Elastic Search!');
+        $json_data = file_get_contents(__DIR__ . '/../../servers/test.json');
+        $I->sendPUT('alex_alex_index/alex_test/1?pretty', [
+            'user' => 'alex',
+            "post_date" => date("Y-m-d H:i:s"),
+            "message" => addslashes($json_data),
+            ]);
+        $I->canSeeResponseCodeIs(201);
+        $I->seeResponseIsJson();
+        $I->comment('The PUT request was sent to Elastic Search!');
 
 
 
