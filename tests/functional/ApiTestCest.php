31b78191c90f545d5e622ede2196bd471cd6c329
Sereda-fazen <fazen7@mail.ru>
2017-06-14 14:10:26 +0300

    remove index - test_index

--- tests/functional/ApiTestCest.php
+++ tests/functional/ApiTestCest.php
@@ -12,43 +12,47 @@ class ApiCest
 
         $I->amHttpAuthenticated('elastic', 'changeme');
         $I->haveHttpHeader('Content-Type', 'application/json');
-        //$I->sendDELETE('alex_index', []);
-        $I->sendPUT('alex_index?pretty', [
-
-          "settings"=> [
-                "number_of_replicas"=> 1,
-            "number_of_shards"=> 1,
-            "analysis"=> [],
-            "refresh_interval"=> "1s"
-          ],
-
-          "mappings"=> [
-                "my_type_name" => [
-                    "properties"=> [
-                        "title"=> [
-                            "type"=> "text",
-                  "analyzer"=> "english"
-                ]
-              ]
-            ]
-          ]
-
-
-        ]);
-
-
-        $I->seeResponseCodeIs(200);
-        $I->seeResponseIsJson();
-
-        $json_data = file_get_contents(__DIR__ . '/../../servers/test.json');
-        $I->sendPUT('alex_index/alex_test/1?pretty', [
-            'user' => 'alex',
-            "post_date" => date("Y-m-d H:i:s"),
-            "message" => addslashes($json_data),
-            "timestamp" => date("Y-m-d H:i:s")]);
-        $I->canSeeResponseCodeIs(201);
-        $I->seeResponseIsJson();
-        $I->comment('The PUT request was sent to Elastic Search!');
+        $I->sendDELETE('alex_index', []);
+//        $I->sendPUT('alex_index?pretty', [
+//
+//          "settings"=> [
+//                "number_of_replicas"=> 1,
+//            "number_of_shards"=> 1,
+//            "analysis"=> [],
+//            "refresh_interval"=> "1s"
+//          ],
+//
+//            "mappings"=> [
+//                    "my_type"=> [
+//                        "_timestamp"=> [
+//                        "enabled"=> true
+//                        ]
+//
+//            ],
+//
+//
+//                    "properties"=> [
+//                        "title"=> [
+//                            "type"=> "text",
+//                  "analyzer"=> "english"
+//                ]
+//              ]
+//            ]
+//        ]);
+//
+//
+//        $I->seeResponseCodeIs(200);
+//        $I->seeResponseIsJson();
+////
+//        $json_data = file_get_contents(__DIR__ . '/../../servers/test.json');
+//        $I->sendPUT('alex_index/alex_test/1?pretty', [
+//            'user' => 'alex',
+//            "post_date" => date("Y-m-d H:i:s"),
+//            "message" => addslashes($json_data),
+//            ]);
+//        $I->canSeeResponseCodeIs(201);
+//        $I->seeResponseIsJson();
+//        $I->comment('The PUT request was sent to Elastic Search!');
 
 
 
