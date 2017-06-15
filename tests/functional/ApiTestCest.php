1ccee86a4cb19e984c2cf91868b73a2aac90031a
Sereda-fazen <fazen7@mail.ru>
2017-06-15 10:36:04 +0300

    remove index - test_index

--- tests/functional/ApiTestCest.php
+++ tests/functional/ApiTestCest.php
@@ -11,10 +11,10 @@ class ApiCest
     {
         $I->amHttpAuthenticated('elastic', 'changeme');
         $I->haveHttpHeader('Content-Type', 'application/json');
-//        //$I->sendDELETE('alex212', []);
-//
-//
-//
+        $I->sendDELETE('alex', []);
+
+
+
 //        $I->sendPUT('alex?pretty', [
 //
 //          "mappings" => [
@@ -32,17 +32,17 @@ class ApiCest
 //
 //        $I->seeResponseCodeIs(200);
 //        $I->seeResponseIsJson();
-
-        $json_data = file_get_contents(__DIR__ . '/../../servers/test.json');
-        $array = json_decode($json_data, TRUE );
-        foreach ($array as $post) {
-            $I->sendPOST('alex/alex_type/2',
-                $post);
-            $I->canSeeResponseCodeIs(201);
-            $I->seeResponseIsJson();
-        }
-        $I->comment('The POST request was sent to Elastic Search!');
-
+//
+//        $json_data = file_get_contents(__DIR__ . '/../../servers/test.json');
+//        $array = json_decode($json_data, TRUE );
+//        foreach ($array as $post) {
+//            $I->sendPOST('alex/alex_type/2',
+//                $post);
+//            $I->canSeeResponseCodeIs(201);
+//            $I->seeResponseIsJson();
+//        }
+//        $I->comment('The POST request was sent to Elastic Search!');
+//
 
 
 
