d39a55021ac127c2be640e36b213a81929ebb170
Sereda-fazen <fazen7@mail.ru>
2017-06-15 09:45:28 +0300

    remove index - test_index

--- tests/functional/ApiTestCest.php
+++ tests/functional/ApiTestCest.php
@@ -9,50 +9,38 @@ class ApiCest
 
     function sendPutRequest(\FunctionalTester $I)
     {
-
         $I->amHttpAuthenticated('elastic', 'changeme');
         $I->haveHttpHeader('Content-Type', 'application/json');
-        $I->sendDELETE('alex212', []);
-        $I->sendDELETE('alex1', []);
-        $I->sendDELETE('alex22', []);
-        $I->sendDELETE('alex222', []);
-        $I->sendDELETE('alex_final', []);
-        $I->sendDELETE('my_index', []);
+//        $I->sendDELETE('alex212', []);
 
 
-//        $I->sendPUT('alex212?pretty', [
-//
-//          "mappings" => [
-//                "alex_test" => [
-//                    "properties" => [
-//                        "date" => [
-//                            "type" => "date"
-//                ]
-//              ]
-//    ]
-//
-//
-//        ],
-//        ]);
-//
-//        $I->seeResponseCodeIs(200);
-//        $I->seeResponseIsJson();
-//
-//        $json_data = file_get_contents(__DIR__ . '/../../servers/test.json');
-//        $I->sendPOST('alex212/alex_test/1?pretty', [
-//
-//            "host" => "Walmart-furniture",
-//            "dashboard" => 2.82,
-//            "content" => 2.26,
-//            "product" => 3.66,
-//            "insight" => 2.3,
-//            "total_time" => 11.04,
-//            "date" => "2017-06-13T14:37:24.123",
-//            "status" => "Success"
-//    ]);
-//        $I->canSeeResponseCodeIs(201);
-//        $I->seeResponseIsJson();
-//        $I->comment('The POST request was sent to Elastic Search!');
+
+        $I->sendPUT('alex?pretty', [
+
+          "mappings" => [
+                "alex_test" => [
+                    "properties" => [
+                        "date" => [
+                            "type" => "date"
+                ]
+              ]
+    ]
+
+
+        ],
+        ]);
+
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+
+        $json_data = file_get_contents(__DIR__ . '/../../servers/test.json');
+        $array = json_decode($json_data);
+      
+        $I->sendPUT('alex/alex_type/1?pretty', [
+            addslashes($array)]);
+        $I->canSeeResponseCodeIs(201);
+        $I->seeResponseIsJson();
+        $I->comment('The POST request was sent to Elastic Search!');
 //
 //
 //
