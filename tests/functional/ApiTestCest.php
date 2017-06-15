bcb30057432358f8457a3c9a9a9ab5d706973d2c
Sereda-fazen <fazen7@mail.ru>
2017-06-15 10:36:45 +0300

    remove index - test_index

--- tests/functional/ApiTestCest.php
+++ tests/functional/ApiTestCest.php
@@ -11,38 +11,38 @@ class ApiCest
     {
         $I->amHttpAuthenticated('elastic', 'changeme');
         $I->haveHttpHeader('Content-Type', 'application/json');
-        $I->sendDELETE('alex', []);
+     //   $I->sendDELETE('alex', []);
 
 
 
-//        $I->sendPUT('alex?pretty', [
-//
-//          "mappings" => [
-//                "alex_test" => [
-//                    "properties" => [
-//                        "date" => [
-//                            "type" => "date"
-//                ]
-//              ]
-//    ]
-//
-//
-//        ],
-//        ]);
-//
-//        $I->seeResponseCodeIs(200);
-//        $I->seeResponseIsJson();
-//
-//        $json_data = file_get_contents(__DIR__ . '/../../servers/test.json');
-//        $array = json_decode($json_data, TRUE );
-//        foreach ($array as $post) {
-//            $I->sendPOST('alex/alex_type/2',
-//                $post);
-//            $I->canSeeResponseCodeIs(201);
-//            $I->seeResponseIsJson();
-//        }
-//        $I->comment('The POST request was sent to Elastic Search!');
-//
+        $I->sendPUT('alex?pretty', [
+
+          "mappings" => [
+                "alex_test" => [
+                    "properties" => [
+                        "date" => [
+                            "type" => "date"
+                ]
+              ]
+    ]
+
+
+        ],
+        ]);
+
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+
+        $json_data = file_get_contents(__DIR__ . '/../../servers/test.json');
+        $array = json_decode($json_data, TRUE );
+        foreach ($array as $post) {
+            $I->sendPOST('alex/alex_type',
+                $post);
+            $I->canSeeResponseCodeIs(201);
+            $I->seeResponseIsJson();
+        }
+        $I->comment('The POST request was sent to Elastic Search!');
+
 
 
 
